3f8a1e29597c6cc8b1eb81717433e36083e8f721
Han Hossain <ahanuf12@gmail.com>
2023-04-24 00:13:07 -0700

    enable change tracking

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -9,7 +9,7 @@ impl Tester {
     pub async fn create_from_env() -> Self {
         dotenv::dotenv().unwrap();
         let password = std::env::var("SQL_SERVER_PASS").unwrap();
-        Tester::create(&password).await.setup().await
+        Tester::create(&password).await
     }
 
     pub async fn get_value(&self, id: &str) -> Option<i32> {
@@ -55,19 +55,22 @@ impl Tester {
             .await
             .unwrap();
 
-        let database_result: Option<(String,)> =
-            sqlx::query_as("SELECT [name] FROM sys.databases WHERE [name] = 'tester'")
-                .fetch_optional(&mut conn)
-                .await
-                .unwrap();
-        if database_result.is_none() {
+        let database_exists = sqlx::query_as::<_, (String,)>(
+            "SELECT [name] FROM sys.databases WHERE [name] = 'tester'",
+        )
+        .fetch_optional(&mut conn)
+        .await
+        .unwrap()
+        .is_some();
+
+        if !database_exists {
             println!("Creating database");
             sqlx::query("CREATE DATABASE tester")
                 .execute(&mut conn)
                 .await
                 .unwrap();
 
-            println!("Enabling change tracking");
+            println!("Enabling change tracking for database");
             sqlx::query(
                 "ALTER DATABASE tester \
 SET CHANGE_TRACKING = ON \
@@ -87,22 +90,20 @@ SET CHANGE_TRACKING = ON \
         .await
         .unwrap();
 
+        if !database_exists {
+            println!("Creating table");
+            sqlx::query("CREATE TABLE pairs (id NVARCHAR(256) PRIMARY KEY, value INT)")
+                .execute(&pool)
+                .await
+                .unwrap();
+
+            println!("Enabling change tracking for table");
+            sqlx::query("ALTER TABLE pairs ENABLE CHANGE_TRACKING")
+                .execute(&pool)
+                .await
+                .unwrap();
+        }
+
         Tester { pool }
-    }
-
-    async fn setup(self) -> Self {
-        sqlx::query(
-            r#"
-    IF NOT EXISTS (
-        SELECT * FROM sys.tables t
-        JOIN sys.schemas s ON (t.schema_id = s.schema_id)
-        WHERE s.name = 'dbo' AND t.name = 'pairs')
-    CREATE TABLE pairs (id NVARCHAR(256) PRIMARY KEY, value INT)"#,
-        )
-        .execute(&self.pool)
-        .await
-        .unwrap();
-
-        self
     }
 }
